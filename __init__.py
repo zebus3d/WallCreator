e493eb20cec3f1d1e3b64d3ea16af81bc9927b46
zebus3d <48990185+zebus3d@users.noreply.github.com>
2021-03-17 13:47:52 +0100

    precision amount bevel
    precision amount bevel

--- __init__.py
+++ __init__.py
@@ -165,7 +165,7 @@ class zwc_properties(PropertyGroup):
     fill_boundaryes: BoolProperty(name="fill_boundaryes", default=True, update=UpdatedFunction)
     first_time: BoolProperty(name="first_time", default=True)
     bevel: BoolProperty(name="bevel", default=False, update=UpdatedFunction)
-    bevel_amount: FloatProperty(name="bevel_amount", min=0.000, max=1.0, default=0.002, update=UpdatedFunction)
+    bevel_amount: FloatProperty(name="bevel_amount", min=0.000, max=1.0, default=0.002, precision=3, update=UpdatedFunction)
 
 
 class MAIN_OT_operator(Operator):
